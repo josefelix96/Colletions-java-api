0300c9cf32fe273d78442c5db47c6b7481455852
José Felix <jf-junior9@hotmail.com.br>
2024-06-25 16:03:02 -0300

    resolvendo desafios Catalogo de livros e Ordenação por pessoa

---
+++ src/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,50 @@
+package Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atrriubuto
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome,idade,altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoaPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorIdade);
+        return pessoaPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoPessoa
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        // Adicionando pessoas à lista
+        ordenacaoPessoa.adicionarPessoa("Alice", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Bob", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Charlie", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("David", 17, 1.56);
+
+        // Exibindo a lista de pessoas adicionadas
+        System.out.println(ordenacaoPessoa.pessoaList);
+
+        // Ordenando e exibindo por idade
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+
+        // Ordenando e exibindo por altura
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
